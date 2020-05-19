1263880c351acaad8dba0917aaf79171262d141b
Devesh Kumar <devesh.ase@gmail.com>
2020-05-18 23:07:38 -0500

    Moved limits-service.properties

--- spring-cloud-config-server/src/main/java/com/deva/chicago/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/deva/chicago/SpringCloudConfigServerApplication.java
@@ -10,6 +10,8 @@ public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+		
+		System.out.println("SpringCloudConfigServerApplication is running..");
 	}
 
 }
